c80d79442bf1e5c841ac203f5ee51cc23bd50d9f
foxdesignstudio <44208745+foxdesignstudio@users.noreply.github.com>
2018-10-16 20:48:21 +0300

    my first request
    16.10.2018

---
+++ request/RESUME/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('.skills').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 2500,
+    smartSpeed: 2500,
+    autoplayHoverPause: true,
+    loop: true,
+    dots: false,
+    items: 3,
+    nav: false,
+  });
+ 
+});
