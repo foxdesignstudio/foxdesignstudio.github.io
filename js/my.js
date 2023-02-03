a1dba95b622405b373ad9403710caee184f83f7d
foxdesignstudio <grayfoxbox@gmail.com>
2023-02-04 02:27:53 +0300

    test 04.02

--- js/my.js
+++ js/my.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+ $(document).ready(function(){
 	//проявление прелодера
 	setTimeout( function(){
 		$('.blind__main').addClass('open')
@@ -48,10 +48,6 @@ $(document).ready(function(){
 		$('.fab').removeClass('spread');
 		return false;
 	});
-	//вкл/выкл анимированного задника сайта
-	$('.small-switcher').click(function(){
-		$('.particles').toggleClass('run');
-	});
 	//фиксация заголовков ссылок меню навигации слева
 	$('.navigator__link--trigger').click(function(){
 		$(this).toggleClass('pin');
@@ -65,11 +61,25 @@ $(document).ready(function(){
 		$(this).parent().addClass('active');
 		return false
 	});
-	//подсветка кнопки превью без нажатия в мобильной версии сайт-селектора
-	$(window).resize(function(){
-		if($(window).width()>550){
-			$('.slide__switcher.toRight').addClass('active');
-		}
+	//скрипт переключения лицевой/обратной сторон любимых банкнот
+	$('.slider-selector .selector__button').click(function(){
+		$(this).parent().parent().find('.slideshow__side').addClass('hide');
+		$(this).siblings().removeClass('active');
+		var id = $(this).attr('href');
+		$(id).removeClass('hide');
+		$(this).addClass('active');
+		return false
+	});
+	//чередование кнопок просмотра аверса/реверса банкноты
+	$('.view-a').click(function(){
+		$(this).parent().find('.selector__viewer-a').removeClass('closed');
+		$(this).parent().find('.selector__viewer-b').addClass('closed');
+		return false
+	});
+	$('.view-b').click(function(){
+		$(this).parent().find('.selector__viewer-a').addClass('closed');
+		$(this).parent().find('.selector__viewer-b').removeClass('closed');
+		return false
 	});
 	//система выбора сайтов портфолио для просмотра
 	$('.portfolio__content a').click(function(){
@@ -131,10 +141,27 @@ $(document).ready(function(){
 			$('.megasquare__reset').addClass('ready')
 		};
 	});
+	//кнопка вкл/выкл экрана поверх мегаквадрата
+	$('.petal__trigger').click(function(){
+		$(this).toggleClass('active');
+		$('.panel.creator .panel__square').toggleClass('hidden');
+		$('.megasquare__petal.megasquare__creator.square-2').toggleClass('hidden');
+		$('.megasquare__screen').toggleClass('active');
+		if($('.megasquare__screen').hasClass('active')){
+			setTimeout(function() {$('.screen__wrapper').addClass('ready')}, 200)
+		} else {
+			$('.screen__wrapper').removeClass('ready')
+		}
+	});
 	//скрипт работы квестора для мобильных
 	$('.questor__item').click(function(){
 		$(this).toggleClass('active');
 	});
+	//появление подсказки на левой части блока превью сайтов
+	$('.slide__right').hover(
+		function(){ $('.pc__preview').addClass('active')},
+		function(){ $('.pc__preview').removeClass('active')}
+	);
 	//смена задника панели мегаквадрата
 	$('.panel__square').hover(function(){
 		$('#background').removeClass().addClass(
@@ -151,20 +178,55 @@ $(document).ready(function(){
 	});
 	//прокрутка мобильного сайта в телефоне
 	$('.scroll-bottom-skinali').click(function(){
-		$('.mobile__image.ui-draggable.ui-draggable-handle').css('top','-4238px');
+		$('.mobile__wrapper.skinali .mobile__image.ui-draggable.ui-draggable-handle').css('top','-4238px');
 	});
 	$('.scroll-bottom-scarlet').click(function(){
-		$('.mobile__image.ui-draggable.ui-draggable-handle').css('top','-1732px');
+		$('.mobile__wrapper.scarlet .mobile__image.ui-draggable.ui-draggable-handle').css('top','-1600px');
+	});
+	$('.scroll-bottom-avtoholl').click(function(){
+		$('.mobile__wrapper.avtoholl .mobile__image.ui-draggable.ui-draggable-handle').css('top','-2482px');
+	});
+	$('.scroll-bottom-capone').click(function(){
+		$('.mobile__wrapper.capone .mobile__image.ui-draggable.ui-draggable-handle').css('top','-3045px');
 	});
 	$('.scroll-top').click(function(){
 		$('.mobile__image.ui-draggable.ui-draggable-handle').css('top','0');
 	});
-	// $('.shutdown').click(function(){
-	// 	$('.mobile__cover').toggleClass('unhold');
-	// });
 	$('.shutdown').click(function(){
 		$(this).parent().parent().find('.mobile__cover').toggleClass('unhold');
 	});
+	//скрипт работы слайдера купюр в мегакубике
+	document.getElementById('slider-left').onclick = sliderLeft;
+	var left = 0;
+	function sliderLeft() {
+		var polosa = document.getElementById('sliderContent');
+		var knopka1 = document.getElementById('slider-left');
+		var knopka2 = document.getElementById('slider-right');
+		left = left - 580;
+		if (left <= 0) {
+			left = 0;
+			knopka1.style.background = '#ccc';
+		} else {
+			knopka1.style.background = '#ff9933';
+			knopka2.style.background = '#ff9933';
+		}
+		polosa.style.left = left +'px';
+	}
+	document.getElementById('slider-right').onclick = sliderRight;
+	function sliderRight() {
+		var polosa = document.getElementById('sliderContent');
+		var knopka1 = document.getElementById('slider-left');
+		var knopka2 = document.getElementById('slider-right');
+		left = left + 580;
+		if (left >= 1160) {
+			left = 1160;
+			knopka2.style.background = '#ccc';
+		} else {
+			knopka1.style.background = '#ff9933';
+			knopka2.style.background = '#ff9933';
+		}
+		polosa.style.left = left +'px';
+	}
 	//скрипт перетаскивания мышью мобильной версии превью
 	$('#drag-1').draggable({
 		drag: function(event, ui) {
@@ -189,6 +251,29 @@ $(document).ready(function(){
 			}
 		}
 	});
+	$('#drag-2').draggable({
+		drag: function(event, ui) {
+			var offset = $(this).offset();
+			var xPos = offset.left;
+			var yPos = offset.top;
+
+		$('#posX').text('x: ' + xPos);
+		$('#posY').text('y: ' + yPos);
+
+			if(ui.position.left>0) {
+				ui.position.left=0;
+			}
+			if(ui.position.left<0) {
+				ui.position.left=0;
+			}
+			if(ui.position.top<-2482) {
+				ui.position.top=-2482;
+			}
+			if(ui.position.top>0) {
+				ui.position.top=0;
+			}
+		}
+	});
 	$('#drag-3').draggable({
 		drag: function(event, ui) {
 			var offset = $(this).offset();
@@ -204,8 +289,31 @@ $(document).ready(function(){
 			if(ui.position.left<0) {
 				ui.position.left=0;
 			}
-			if(ui.position.top<-1732) {
-				ui.position.top=-1732;
+			if(ui.position.top<-1600) {
+				ui.position.top=-1600;
+			}
+			if(ui.position.top>0) {
+				ui.position.top=0;
+			}
+		}
+	});
+	$('#drag-4').draggable({
+		drag: function(event, ui) {
+			var offset = $(this).offset();
+			var xPos = offset.left;
+			var yPos = offset.top;
+
+		$('#posX').text('x: ' + xPos);
+		$('#posY').text('y: ' + yPos);
+
+			if(ui.position.left>0) {
+				ui.position.left=0;
+			}
+			if(ui.position.left<0) {
+				ui.position.left=0;
+			}
+			if(ui.position.top<-3045) {
+				ui.position.top=-3045;
 			}
 			if(ui.position.top>0) {
 				ui.position.top=0;
@@ -284,4 +392,29 @@ $(document).ready(function(){
 			}
 		}
 	});
+	//прокрутка сайта 4 в окне превью для пк
+	$('#sitePreview-4').draggable({
+		drag: function(event, ui) {
+			var offset = $(this).offset();
+			var xPos = offset.left;
+			var yPos = offset.top;
+
+		$('#posX').text('x: ' + xPos);
+		$('#posY').text('y: ' + yPos);
+
+			if(ui.position.left>0) {
+				ui.position.left=0;
+			}
+			if(ui.position.left<0) {
+				ui.position.left=0;
+			}
+			if(ui.position.top<-535) {
+				ui.position.top=-535;
+			}
+			if(ui.position.top>0) {
+				ui.position.top=0;
+			}
+		}
+	});
+
 });
